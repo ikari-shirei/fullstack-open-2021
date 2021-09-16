138bfb183f58e58753100e5966d906d083f199cc
ikari-shirei <algrsyu@protonmail.com>
2021-09-16 23:32:57 +0300

    Add 4.2 blog list

---
+++ part4/bloglist/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+const mongoUrl = config.MONGODB_URI
+
+logger.info('connecting to ' + config.MONGODB_URI)
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
